0d4db44afb90438ab098194d8f522d52789e6d8d
Renato Dell'Osso <rddellosso4@gmail.com>
2025-09-05 17:02:26 -0400

    More schema tests

---
+++ tests/schema.test.cpp
@@ -0,0 +1,69 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../src/schema.h"
+
+
+TEST_CASE("returns true for valid schema", "[isValidSchema]") {
+  std::unordered_map<char*, int> nameToIndex;
+  nameToIndex[(char*)"id"] = 0;
+
+  Schema schema = {
+    1,
+    new ValueType[1] {
+      VT_INT
+    },
+    nameToIndex
+  };
+
+  REQUIRE(isValidSchema(schema));
+}
+
+TEST_CASE("returns false for schema with 0 column count", "[isValidSchema]") {
+  std::unordered_map<char*, int> nameToIndex;
+  nameToIndex[(char*)"id"] = 0;
+
+  Schema schema = {
+    0,
+    new ValueType[1] {
+      VT_INT
+    },
+    nameToIndex
+  };
+
+  REQUIRE(!isValidSchema(schema));
+}
+
+TEST_CASE("returns false for schema with an ID column of the wrong type", "[isValidSchema]") {
+  std::unordered_map<char*, int> nameToIndex;
+  nameToIndex[(char*)"id"] = 0;
+
+  Schema schema = {
+    0,
+    new ValueType[1] {
+      VT_FLOAT
+    },
+    nameToIndex
+  };
+
+  REQUIRE(!isValidSchema(schema));
+
+  schema.colTypes[0] = VT_STRING;
+  REQUIRE(!isValidSchema(schema));
+}
+
+TEST_CASE("returns false for schema with an ID column in the wrong index", "[isValidSchema]") {
+  std::unordered_map<char*, int> nameToIndex;
+  nameToIndex[(char*)"id"] = 1;
+
+  Schema schema = {
+    0,
+    new ValueType[1] {
+      VT_FLOAT
+    },
+    nameToIndex
+  };
+
+  REQUIRE(!isValidSchema(schema));
+
+  schema.nameToIndex = std::unordered_map<char*, int>();
+  REQUIRE(!isValidSchema(schema));
+}
